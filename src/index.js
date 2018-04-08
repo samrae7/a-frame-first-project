d1d6ed622a112329591fc3054920ef666d61495d
Sam Rae <samuelrae@gmail.com>
2018-04-08 23:33:19 +0100

    addded gamepad controls

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import {aframe} from "aframe";
+import * as AFRAME from "aframe";
 import * as physics from "aframe-physics-system";
 import * as eventSet from "aframe-event-set-component";
 import * as environment from "aframe-environment-component";
+import * as GamepadControls from "aframe-gamepad-controls";
+AFRAME.registerComponent('gamepad-controls', GamepadControls);
